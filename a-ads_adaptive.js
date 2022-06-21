52de8fcb40a97f0276e5f3aa5e40e33b5bce1747
DirectLinkz <107291570+DirectLinkz@users.noreply.github.com>
2022-06-21 11:14:14 +0700

    Add files via upload

---
+++ a-ads_adaptive.js
@@ -0,0 +1,3 @@
+//<![CDATA[
+document.write('<iframe data-aa='2031705' src='//acceptable.a-ads.com/2031705' style='border:0px; padding:0; width:100%; height:100%; overflow:hidden; background-color: transparent;'></iframe>');
+//]]>
